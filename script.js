7e0986b88d00621b58b76a70e51f6f9b7390797f
unknown <renanxmf@outlook.com>
2022-03-25 16:29:35 -0300

    resolvendo

---
+++ script.js
@@ -0,0 +1,35 @@
+// const h1element= document.querySelector("h1");
+
+// h1element.style.backgroundColor="red";
+// h1element.style.color="white";
+// h1element.style.textAlign="center";
+
+// const elementop= document.querySelector(".paragrafo");
+// elementop.style.backgroundColor="violet";
+// elementop.style.color="blue";
+// elementop.style.fontSize="30px"
+
+// const pa2= document.querySelector("#pa-dois");
+// pa2.style.backgroundColor="black";
+// pa2.style.color="white";
+
+// const elemetoh1=document.createElement("h1");
+// const body=document.querySelector("body");
+// elemetoh1.textContent=("Hello JS");
+// body.append(elemetoh1)
+// body.insertAdjacentElement("afterbegin",elemetoh1)
+
+
+
+// btn1=addEventListener("click", function(event){
+//     body.classList.toggle("azul");
+//     btn1.classList.toggle("azul");
+
+// })
+
+const btn1= document.querySelector(".btn1");
+const body=document.querySelector("body");
+const img=document.querySelector(".imagem");
+
+const imagemSrc='https://crocoder.dev/icon.png';
+img.src=imagemSrc;
